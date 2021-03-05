f2c7782700ffc8b666a1568c30d4f8390cb0b850
shiv-chan <kshibuya31@gmail.com>
2021-03-05 17:00:45 +0900

    Update script.
    Set the limitaion of the number of data fetching.
    Comment out 'load' eventlistener, and set 'updateData' function inside of fetch function.

--- Web Development II/weekly assginment 3/script.js
+++ Web Development II/weekly assginment 3/script.js
@@ -6,28 +6,22 @@ const noFound = document.querySelector('.noFound');
 
 const photos = [];
 
-fetch('https://jsonplaceholder.typicode.com/photos')
+fetch('https://jsonplaceholder.typicode.com/photos?_limit=500')
 	.then((res) => {
 		if (res.status !== 200) {
 			console.log(`Error: ${res.status}`);
 		}
 		return res.json();
 	})
-	.then((data) => photos.push(...data))  //pass all data to photos array
+	.then((data) => photos.push(...data)) //pass all data to photos array
+	.then(() => updateData(photos))
 	.catch((err) => console.log(`Error: ${err}`));
 
-function showData() {
-	const tableData = photos.map((photo) => {
-			return `<tr><td class="id">${photo.id}</td><td class="albumId">${photo.albumId}</td><td class="title">${photo.title}</td><td class="image"><img src="${photo.url}"></td><td class="thumbnail"><img src="${photo.thumbnailUrl}"></td></tr>`;
-		}).join('');
-
-	tBody.innerHTML = tableData;
-}
 
 function updateData(arr) {
 	const tableData = arr.map((photo) => {
-      const regex = new RegExp(`^${input.value}`); //begin-with match
-      const photoId = photo.id.toString().replace(regex, `<span class='hl'>${input.value}</span>`); //highlight matches
+			const regex = new RegExp(`^${input.value}`); //begin-with match
+			const photoId = photo.id.toString().replace(regex, `<span class='hl'>${input.value}</span>`); //highlight matches
 
 			return `<tr><td class="id">${photoId}</td><td class="albumId">${photo.albumId}</td><td class="title">${photo.title}</td><td class="image"><img src="${photo.url}"></td><td class="thumbnail"><img src="${photo.thumbnailUrl}"></td></tr>`;
 		})
@@ -37,7 +31,7 @@ function updateData(arr) {
 }
 
 //onload
-window.addEventListener('load', showData);
+//window.addEventListener('load', showData);
 
 //click button
 btn.addEventListener('click', function (e) {
@@ -45,16 +39,16 @@ btn.addEventListener('click', function (e) {
 	const inputPhotoId = parseInt(input.value);
 
 	const filteredPhotos = photos.filter((photo) => {
-    const regex = new RegExp(`^${input.value}`); //begin-with match
+		const regex = new RegExp(`^${input.value}`); //begin-with match
 		return photo.id.toString().match(regex);
 	});
-  
+
 	if (filteredPhotos.length !== 0) {
-    updateData(filteredPhotos);
+		updateData(filteredPhotos);
 		noFound.classList.remove('show');
 	} else if (filteredPhotos.length <= 0) {
-    updateData(filteredPhotos);
+		updateData(filteredPhotos);
 		noFound.classList.add('show');
 	}
-  document.form.reset();
+	document.form.reset();
 });
