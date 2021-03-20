b3ffb512b7ddd106bad5c507407130830b604e6e
shiv-chan <kshibuya31@gmail.com>
2021-03-20 16:09:35 +0900

    Add history search and its graph.

--- Web Development II/weekly-assignment-4/script.js
+++ Web Development II/weekly-assignment-4/script.js
@@ -10,6 +10,10 @@ const displayTo = document.querySelector('.to');
 const fromAmount = document.querySelector('.fromAmount');
 const toAmount = document.querySelector('.toAmount');
 const main = document.querySelector('main');
+let fromCurr;
+let toCurr;
+let base;
+let rateKey;
 
 window.onload = setOptions;
 
@@ -26,10 +30,12 @@ const fetchCurrencies = async() => {
     const data = await res.json();
     Object.keys(data.response.fiats).map((key) => currencies.push(data.response.fiats[key]))
   } catch(err) {
+    fetchCurrencies();
     console.log(`Error1: ${err}`);
   }
 }
 
+//set options for select input
 async function setOptions() {
   await fetchCurrencies();
 
@@ -43,45 +49,40 @@ async function setOptions() {
   }
 }
 
+//get currency from selections
+function getCurrency(){
+  //selected currencies
+  fromCurr = selects[0].selectedOptions[0].textContent;
+  toCurr = selects[1].selectedOptions[0].textContent;
+  
+  //get currency code according to currency name
+  if(fromCurr !== ''){
+    const index = currencies.findIndex((obj) => obj.currency_name == fromCurr);
+    base = currencies[index].currency_code;
+  }
+
+  if(toCurr !== ''){
+    const index = currencies.findIndex((obj) => obj.currency_name == toCurr);
+    rateKey = currencies[index].currency_code;
+  }
+}
+
 //convert
 async function convert() {
-  let fromCurr;
-  let toCurr;
-  let base;
-  let rateKey;
+  await getCurrency();
 
-  function getCurrency(){
-      //selected currencies
-      fromCurr = selects[0].selectedOptions[0].textContent;
-      toCurr = selects[1].selectedOptions[0].textContent;
-      
-      //get currency code according to currency name
-      if(fromCurr !== ''){
-        const index = currencies.findIndex((obj) => obj.currency_name == fromCurr);
-        base = currencies[index].currency_code;
+  try {const res = await fetch(`https://currencyscoop.p.rapidapi.com/latest?base=${base}`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": apikey,
+        "x-rapidapi-host": "currencyscoop.p.rapidapi.com"
       }
-
-      if(toCurr !== ''){
-        const index = currencies.findIndex((obj) => obj.currency_name == toCurr);
-        rateKey = currencies[index].currency_code;
-      }
-    }
-
-      await getCurrency();
-
-      try {const res = await fetch(`https://currencyscoop.p.rapidapi.com/latest?base=${base}`, {
-          "method": "GET",
-          "headers": {
-            "x-rapidapi-key": apikey,
-            "x-rapidapi-host": "currencyscoop.p.rapidapi.com"
-          }
-        });
+    });
 
     const data = await res.json();
     const rate = data.response.rates[rateKey];
     
     //display results
-
     displayFrom.textContent = ` ${fromCurr}`;
     fromAmount.textContent = formatNumber(fromInput.value);
     displayTo.textContent = ` ${toCurr}`;
@@ -90,6 +91,7 @@ async function convert() {
     main.style.display = 'flex';
     
   } catch(err) {
+    convert();
     console.log(`Error2: ${err}`);
   }
 }
@@ -110,5 +112,76 @@ function formatNumber(num) {
   return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
 }
 
+const startDate = document.querySelector('#startDate');
+const endDate = document.querySelector('#endDate');
+const showHistoryBtn = document.querySelector('.showHistory');
+
+//get days array
+let days = [];
+const getDaysArray = (start, end) => {
+  for(let dt = new Date(start); dt <= end; dt.setDate(dt.getDate() + 1)){
+      days.push(new Date(dt).toISOString().split("T")[0]);
+  }
+  console.log(days);
+};
+
+
+//map days and fetch each data
+let rateHistory = [];
+const fetchHistory = async(eachDay) => {
+  await getCurrency();
+
+  try {
+    const res = await fetch(`https://currencyscoop.p.rapidapi.com/historical?base=${base}&date=${eachDay}`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": apikey,
+        "x-rapidapi-host": "currencyscoop.p.rapidapi.com"
+      }
+    });
+
+    const data = await res.json();
+    if(data.response.rates[rateKey] !== undefined){
+      rateHistory.push(data.response.rates[rateKey]);
+    } else {
+      fetchHistory();
+    }
+    
+  } catch(err) {
+    console.log(`Error3: ${err}`);
+  }
+}
+
+let DateRate = [];
+const combineArr = (date, rate) => {
+  DateRate = [['Date', 'Rate']];
+  for(let i = 0; i < date.length; i++){
+    DateRate.push([date[i], rate[i]]);
+  }
+}
+
 convertBtn.addEventListener('click', convert);
 switchBtn.addEventListener('click', switchCurr);
+showHistoryBtn.addEventListener('click', () => {
+  getDaysArray(new Date(startDate.value), new Date(endDate.value));
+  days.map(async (day) => await fetchHistory(day));
+  combineArr(days, rateHistory);
+  days = [];
+  rateHistory = [];
+  google.charts.load('current', {'packages':['corechart']});
+  google.charts.setOnLoadCallback(drawChart);
+});
+
+//google chart
+function drawChart() {
+  var data = google.visualization.arrayToDataTable(DateRate);
+
+  var options = {
+    title: `${selects[0].selectedOptions[0].textContent} / ${selects[1].selectedOptions[0].textContent}`,
+    hAxis: {title: 'Date', titleTextStyle: {color: '#333'}},
+    vAxis: {title: 'Rate', minValue: 0}
+  };
+
+  var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
