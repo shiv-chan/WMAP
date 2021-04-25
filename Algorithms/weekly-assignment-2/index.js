e28ded053741f08ac09bd78c5817552b35e77bc1
Daniel Corner <danielcorner7@gmail.com>
2021-04-24 22:29:57 -0400

    eliminate magic number, abstract some repetition

--- Algorithms/weekly-assignment-2/index.js
+++ Algorithms/weekly-assignment-2/index.js
@@ -1,100 +1,107 @@
 //Create seat icons when the page is loaded.
-window.addEventListener('DOMContentLoaded', addSeatIcons);
+window.addEventListener("DOMContentLoaded", addSeatIcons);
+
+const NUM_ROWS = 6;
+
+const SEAT_ICON_DIV = `<div class="seat-icon"></div>`;
+const ROW_WITH_TWO_SEATS_DIV = `<div class="row">${SEAT_ICON_DIV}${SEAT_ICON_DIV}`;
+const ROW_WITH_FOUR_SEATS_DIV = `<div class="row">${SEAT_ICON_DIV}${SEAT_ICON_DIV}${SEAT_ICON_DIV}${SEAT_ICON_DIV}`;
 
 /**
  * Create and add seat icons
  */
 function addSeatIcons() {
-	const leftSide = document.querySelector('.left-side');
-	const rightSide = document.querySelector('.right-side');
-	const middle = document.querySelector('.middle');
-	const rowSide = document.createElement('div');
-	rowSide.classList.add('row');
+  // -- grab the elements --
+  const leftSide = document.querySelector(".left-side");
+  const rightSide = document.querySelector(".right-side");
+  const middle = document.querySelector(".middle");
 
-	for (let i = 0; i < 6; i++) {
-		leftSide.insertAdjacentHTML(
-			'beforeend',
-			'<div class="row"><div class="seat-icon"></div><div class="seat-icon"></div></div>'
-		);
-		rightSide.insertAdjacentHTML(
-			'beforeend',
-			'<div class="row"><div class="seat-icon"></div><div class="seat-icon"></div></div>'
-		);
-		middle.insertAdjacentHTML(
-			'beforeend',
-			'<div class="row"><div class="seat-icon"></div><div class="seat-icon"></div><div class="seat-icon"></div><div class="seat-icon"></div></div>'
-		);
-	}
+  // create a row div
+  const rowSide = document.createElement("div");
+  rowSide.classList.add("row");
 
-	const seatIcons = document.getElementsByClassName('seat-icon');
+  // add rows (subtract 1 because there's already one on the page)
+  for (let i = 0; i < NUM_ROWS - 1; i++) {
+    // TODO: try this instead?
+    // leftSide.innerHTML = leftSide.innerHTML + `${ROW_WITH_TWO_SEATS_DIV}`
 
-	//add unique id to all seats
-	for (let i = 0; i < seatIcons.length; i++) {
-		seatIcons[i].setAttribute('id', `${i}`);
-	}
+    leftSide.insertAdjacentHTML("beforeend", `${ROW_WITH_TWO_SEATS_DIV}</div>`);
+    rightSide.insertAdjacentHTML(
+      "beforeend",
+      `${ROW_WITH_TWO_SEATS_DIV}</div>`
+    );
+    middle.insertAdjacentHTML("beforeend", `${ROW_WITH_FOUR_SEATS_DIV}</div>`);
+  }
 
-	const selectedNums = document.querySelector('.selected-nums');
-	const sumPrice = document.querySelector('.sum-price');
-	const dropdownList = document.querySelector('#movie');
-	let displaySeatNum = 0;
-	let displayPrice = 0;
-	let occupiedIndex = JSON.parse(localStorage.getItem('selectedSeatsId')) || [];
+  const seatIcons = document.getElementsByClassName("seat-icon");
 
-	//Set occupied seats
-	for (let index of occupiedIndex) {
-		seatIcons[index].classList.add('occupied');
-	}
+  //add unique id to all seats
+  for (let i = 0; i < seatIcons.length; i++) {
+    seatIcons[i].setAttribute("id", `${i}`);
+  }
 
-	/**
-	 * Change the color when the icon's selected.
-	 * Increase the number of seats and a price.
-	 * Set a selected seats' id to the local storage.
-	 */
-	function selectedIcon() {
-		let price = parseInt(dropdownList.selectedOptions[0].dataset.price);
-		let selectedSeatsId = this.getAttribute('id');
-		this.classList.toggle('selected');
+  const selectedNums = document.querySelector(".selected-nums");
+  const sumPrice = document.querySelector(".sum-price");
+  const dropdownList = document.querySelector("#movie");
+  let displaySeatNum = 0;
+  let displayPrice = 0;
+  let occupiedIndex = JSON.parse(localStorage.getItem("selectedSeatsId")) || [];
 
-		if (this.classList.contains('selected')) {
-			displaySeatNum++;
-			displayPrice += price;
-			occupiedIndex.push(selectedSeatsId);
-		} else {
-			displaySeatNum--;
-			displayPrice -= price;
-		}
-		selectedNums.textContent = displaySeatNum;
-		sumPrice.textContent = displayPrice;
+  //Set occupied seats
+  for (let index of occupiedIndex) {
+    seatIcons[index].classList.add("occupied");
+  }
 
-		//deduplicate the selected seats id
-		let dataStored = occupiedIndex.filter(
-			(value, index, array) => array.indexOf(value) == index
-		);
-		localStorage.setItem('selectedSeatsId', JSON.stringify(dataStored));
-	}
+  /**
+   * Change the color when the icon's selected.
+   * Increase the number of seats and a price.
+   * Set a selected seats' id to the local storage.
+   */
+  function selectedIcon() {
+    let price = parseInt(dropdownList.selectedOptions[0].dataset.price);
+    let selectedSeatsId = this.getAttribute("id");
+    this.classList.toggle("selected");
 
-	/**
-	 * Reset all selected seats to n/a and the paragraph
-	 */
-	function reset() {
-		for (let i = 3; i < seatIcons.length; i++) {
-			if (seatIcons[i].classList.contains('selected')) {
-				seatIcons[i].classList.remove('selected');
-			}
-			displaySeatNum = 0;
-			displayPrice = 0;
-			selectedNums.textContent = displaySeatNum;
-			sumPrice.textContent = displayPrice;
-		}
-	}
+    if (this.classList.contains("selected")) {
+      displaySeatNum++;
+      displayPrice += price;
+      occupiedIndex.push(selectedSeatsId);
+    } else {
+      displaySeatNum--;
+      displayPrice -= price;
+    }
+    selectedNums.textContent = displaySeatNum;
+    sumPrice.textContent = displayPrice;
 
-	//Seat icons clicked
-	for (let i = 3; i < seatIcons.length; i++) {
-		if (!seatIcons[i].classList.contains('occupied')) {
-			seatIcons[i].addEventListener('click', selectedIcon);
-		}
-	}
+    //deduplicate the selected seats id
+    let dataStored = occupiedIndex.filter(
+      (value, index, array) => array.indexOf(value) == index
+    );
+    localStorage.setItem("selectedSeatsId", JSON.stringify(dataStored));
+  }
 
-	//Drop down list changed
-	dropdownList.addEventListener('change', reset);
+  /**
+   * Reset all selected seats to n/a and the paragraph
+   */
+  function reset() {
+    for (let i = 3; i < seatIcons.length; i++) {
+      if (seatIcons[i].classList.contains("selected")) {
+        seatIcons[i].classList.remove("selected");
+      }
+      displaySeatNum = 0;
+      displayPrice = 0;
+      selectedNums.textContent = displaySeatNum;
+      sumPrice.textContent = displayPrice;
+    }
+  }
+
+  //Seat icons clicked
+  for (let i = 3; i < seatIcons.length; i++) {
+    if (!seatIcons[i].classList.contains("occupied")) {
+      seatIcons[i].addEventListener("click", selectedIcon);
+    }
+  }
+
+  //Drop down list changed
+  dropdownList.addEventListener("change", reset);
 }
