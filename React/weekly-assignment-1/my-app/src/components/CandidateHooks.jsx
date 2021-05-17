0d97beeb106009c4e653d6b7aee787419560d0f1
shiv-chan <kshibuya31@gmail.com>
2021-05-17 20:54:19 +0900

    Style the page

--- React/weekly-assignment-1/my-app/src/components/CandidateHooks.jsx
+++ React/weekly-assignment-1/my-app/src/components/CandidateHooks.jsx
@@ -7,11 +7,11 @@ export default function ManyCandidates() {
 	return (
 		<div className="container">
 			<h1>Vote Your Language!</h1>
-			<section>
+			<main>
 				{['Php', 'Python', 'Go', 'Java'].map((lang) => (
 					<Candidate key={lang} lang={lang} />
 				))}
-			</section>
+			</main>
 		</div>
 	);
 }
@@ -19,7 +19,7 @@ export default function ManyCandidates() {
 function Candidate({ lang }) {
 	const [votes, setVotes] = useState(0);
 	return (
-		<section>
+		<section className="candidate">
 			<span className="votes">{votes}</span>
 			<h1>{lang}</h1>
 			<button onClick={() => setVotes(votes + 1)}>Click Here</button>
